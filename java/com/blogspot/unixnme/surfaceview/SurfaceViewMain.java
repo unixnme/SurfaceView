bc2e2b6210389bac3eeaa4d1276b430f54edc77b
Unix&Me <unixnme@gmail.com>
2017-01-22 11:05:34 -0800

    minimal implmentation of camera preview

--- java/com/blogspot/unixnme/surfaceview/SurfaceViewMain.java
+++ java/com/blogspot/unixnme/surfaceview/SurfaceViewMain.java
@@ -1,15 +1,19 @@
 package com.blogspot.unixnme.surfaceview;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.io.IOException;
+
 public class SurfaceViewMain extends Activity implements SurfaceHolder.Callback {
 
-    public final String TAG = "SurfaceActivity";
+    private static final String TAG = SurfaceViewMain.class.getSimpleName();
     private SurfaceView surfaceView;
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +23,33 @@ public class SurfaceViewMain extends Activity implements SurfaceHolder.Callback 
         surfaceView.getHolder().addCallback(this);
     }
 
+    protected void onResume() {
+        super.onResume();
+        if (camera != null)
+            camera.startPreview();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+        }
+    }
+
     public void surfaceCreated(SurfaceHolder holder) {
         Log.i(TAG, "surface created");
+        camera = Camera.open();
+        if (camera != null) {
+            camera.setDisplayOrientation(90);
+            try {
+                camera.setPreviewDisplay(holder);
+            } catch (IOException ie) {
+
+            }
+        }
+        if (camera != null)
+            camera.startPreview();
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
