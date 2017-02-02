1d75221d76126ea7ccc459673f3dc75de5aca6bc
Unix&Me <unixnme@gmail.com>
2017-02-02 22:06:31 +0900

    adding app icon link

--- java/com/blogspot/unixnme/takeiteasy/CreditActivity.java
+++ java/com/blogspot/unixnme/takeiteasy/CreditActivity.java
@@ -11,6 +11,8 @@ public class CreditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.credit_layout);
         TextView switch_camera_link = (TextView) findViewById(R.id.switch_camera_icon_text);
+        TextView app_icon_link = (TextView) findViewById(R.id.app_icon_text);
         switch_camera_link.setMovementMethod(LinkMovementMethod.getInstance());
+        app_icon_link.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
