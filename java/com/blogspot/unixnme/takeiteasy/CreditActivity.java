5e6a58a8aed782960f0c76b167243117a3f2d104
Unix&Me <unixnme@gmail.com>
2017-02-04 07:34:44 +0900

    fix android support device issue; add privacy policy and instructions on the credit page

--- java/com/blogspot/unixnme/takeiteasy/CreditActivity.java
+++ java/com/blogspot/unixnme/takeiteasy/CreditActivity.java
@@ -12,7 +12,9 @@ public class CreditActivity extends AppCompatActivity {
         setContentView(R.layout.credit_layout);
         TextView switch_camera_link = (TextView) findViewById(R.id.switch_camera_icon_text);
         TextView app_icon_link = (TextView) findViewById(R.id.app_icon_text);
+        TextView privacy = (TextView) findViewById(R.id.privacy_policy);
         switch_camera_link.setMovementMethod(LinkMovementMethod.getInstance());
         app_icon_link.setMovementMethod(LinkMovementMethod.getInstance());
+        privacy.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
